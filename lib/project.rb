489a91d278a28e632c6756736ad09461ef482910
storres8 <stevetorres7562@gmail.com>
2018-06-28 13:30:17 -0400

    done.

--- lib/project.rb
+++ lib/project.rb
@@ -3,10 +3,11 @@ attr_accessor :title, :backers
 
 def initialize (title)
   @title = title
-  @backers =[]
+  @backers = []
 end
 
   def add_backer(backer)
-    backers << backer 
+    @backers << backer
+    backer.backed_projects << self
   end
 end
